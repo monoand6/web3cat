e7d45fd6c14bd3c23dfb4e0215547b578d58b3a8
mono6 <monowhile6@gmail.com>
2022-09-14 11:50:22 +0200

    finished tests for blocks

--- probe/blocks.py
+++ probe/blocks.py
@@ -22,46 +22,49 @@ class Blocks:
             self._block_time_est = 13.0
 
     def get_latest_block(self) -> Block:
-        return self._get_block()
+        return self.get_block()
 
     def get_block_right_after_timestamp(self, timestamp: int) -> Block | None:
         ts = timestamp
 
         # right_block is guaranteed to be after the timestamp
-        right_block = self._blocks_db.get_block_after_timestamp(ts)
+        right_block = self._blocks_db.get_block_after_timestamp(ts, self._chain_id)
         if not right_block:
-            right_block = self._get_block()
+            right_block = self.get_block()
 
         if right_block.timestamp < timestamp:  # no blocks exist after the timestamp
             return None
 
-        left_block = self._blocks_db.get_block_before_timestamp(ts)
+        left_block = self._blocks_db.get_block_before_timestamp(ts, self._chain_id)
         if not left_block:
             # harsh approximation but once it's run a single time
             # a better approximation from db will come on each
             # subsequent call
-            left_block = self._get_block(1)
+            left_block = self.get_block(1)
+
+        # Time stamp is before the chain genesis
+        if left_block.timestamp >= timestamp:
+            return left_block
 
         # initial esitmates for blocks are set
         # invariant: left_block.timestamp < timestamp <= right_block.timestamp
         while right_block.number - left_block.number > 1:
             num = (right_block.number + left_block.number) // 2
-            block = self._get_block(num)
+            block = self.get_block(num)
             if block.timestamp >= timestamp:
                 right_block = block
             else:
                 left_block = block
         return right_block
 
-    def _get_block(self, number: int | None = None) -> Block | None:
+    def get_block(self, number: int | None = None) -> Block | None:
         """
         number = None => fetch latest
         """
-        block = None
         if number:
-            block = self._blocks_db.read_blocks(number, self._chain_id)
-        if block is not None:
-            return block
+            blocks = self._blocks_db.read_blocks(number, self._chain_id)
+            if len(blocks) > 0:
+                return blocks[0]
         raw_block = None
         try:
             raw_block = self._w3.eth.get_block(number or "latest")
--- test/test_blocks.py
+++ test/test_blocks.py
@@ -12,4 +12,38 @@ import time
 def test_get_latest_block(blocks: Blocks):
     block = blocks.get_latest_block()
     assert block
-    assert time.time() - block.timestamp < 60
+    assert time.time() - block.timestamp < 600
+
+
+def test_get_block(blocks: Blocks):
+    block = blocks.get_block()
+    num = block.number - 100
+    block = blocks.get_block(num)
+    assert block.number == num
+    # cache
+    block = blocks.get_block(num)
+    assert block.number == num
+
+
+def test_get_block_right_after_timestamp_now(blocks: Blocks):
+    now = time.time()
+    block = blocks.get_block_right_after_timestamp(now)
+    assert not block
+
+
+def test_get_block_right_after_timestamp_before_ethereum(blocks: Blocks):
+    block = blocks.get_block_right_after_timestamp(1347788073)
+    assert block
+    assert block.number == 1
+
+
+def test_get_block_right_after_timestamp(blocks: Blocks):
+    ts = time.time() - 86400
+
+    block = blocks.get_block_right_after_timestamp(ts)
+    prev_block = blocks.get_block(block.number - 1)
+    assert prev_block.timestamp < ts
+    assert block.timestamp >= ts
+
+    block1 = blocks.get_block_right_after_timestamp(block.timestamp)
+    assert block1.number == block.number
