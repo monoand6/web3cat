4e1ab0ef5cc27a86579a505ed68ffcaf0f368f3f
mono6 <monowhile6@gmail.com>
2022-09-27 18:23:14 +0100

    added hypo repo test

--- test/events/test_events_repo.py
+++ test/events/test_events_repo.py
@@ -1,10 +1,14 @@
+from hypothesis import given, settings, HealthCheck
+
 from events.strategies import event
 from probe.events.repo import EventsRepo
 from probe.events.event import Event
 
 
-def test_read_write(events_repo: EventsRepo):
-    e1 = event().example()
+@settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+@given(event=event())
+def test_read_write(event: Event, events_repo: EventsRepo):
+    e1 = event
     e2 = Event(**e1.__dict__)
     e2.block_number += 10
     e2.log_index += 1
@@ -13,3 +17,4 @@ def test_read_write(events_repo: EventsRepo):
     assert sorted(events, key=lambda x: x.block_number) == sorted(
         [e1, e2], key=lambda x: x.block_number
     )
+    events_repo._db.rollback()
