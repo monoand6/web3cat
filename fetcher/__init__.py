c80acf49c302b567dfe04fa3346407e6945f60cb
mono6 <monowhile6@gmail.com>
2022-10-06 22:15:20 +0400

    update

--- fetcher/__init__.py
+++ fetcher/__init__.py
@@ -5,16 +5,16 @@ caches it for subsequent queries.
 Every blockchain query must use this module for efficiency.
 There are several services that do exactly this:
 
-+-------------------------------------------+---------------------------+
-| Service                                   | Description               |
-+===========================================+===========================+
-| :class:`fetcher.events.EventsService`     | Fetching Ethereum events  |
-+-------------------------------------------+---------------------------+
-| :class:`fetcher.events.BlocksService`     | Fetching blocks metadata  |
-+-------------------------------------------+---------------------------+
-| :class:`fetcher.events.ERC20MetasService` | Fetching token metadata   |
-|                                           | (decimals, symbol, name)  |
-+-------------------------------------------+---------------------------+
++------------------------------------------------+---------------------------+
+| Service                                        | Description               |
++================================================+===========================+
+| :class:`fetcher.events.EventsService`          | Fetching Ethereum events  |
++------------------------------------------------+---------------------------+
+| :class:`fetcher.blocks.BlocksService`          | Fetching blocks metadata  |
++------------------------------------------------+---------------------------+
+| :class:`fetcher.erc20_metas.ERC20MetasService` | Fetching token metadata   |
+|                                                | (decimals, symbol, name)  |
++------------------------------------------------+---------------------------+
 
 The best way to get started is to explore these services and module
 docs.
