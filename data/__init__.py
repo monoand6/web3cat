450e0c8a07f18e98d4baf229ddf4d4d83bc633e2
mono6 <monowhile6@gmail.com>
2022-10-11 16:58:19 +0400

    added balances

---
+++ data/__init__.py
@@ -0,0 +1,3 @@
+import polars as pl
+
+pl.Config.set_fmt_str_lengths(44)
--- data/erc20s/erc20_data.py
+++ data/erc20s/erc20_data.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
+from bisect import bisect
 import json
 import os
+import numpy as np
 from typing import Any, Dict, List, Tuple
 from fetcher.blocks.service import DEFAULT_BLOCK_TIMESTAMP_GRID
 from fetcher.erc20_metas import ERC20MetasService
 from fetcher.erc20_metas import erc20_meta
 from fetcher.events import EventsService, Event
 from fetcher.blocks import BlocksService
+from fetcher.calls import CallsService
 import polars as pl
 from web3.contract import Contract
 from web3 import Web3
@@ -28,12 +31,16 @@ class ERC20Data:
     _token: str
     _erc20_metas_service: ERC20MetasService
     _events_service: EventsService
+    _blocks_service: BlocksService
+    _calls_service: CallsService
     _w3: Web3
     _grid_step: int
     _address_filter: List[str]
+    _chain_id: int | None
 
     _meta: ERC20Meta | None
     _transfers: pl.DataFrame | None
+    _token_contract: Contract | None
 
     def __init__(
         self,
@@ -41,6 +48,7 @@ class ERC20Data:
         erc20_metas_service: ERC20MetasService,
         events_service: EventsService,
         blocks_service: BlocksService,
+        calls_service: CallsService,
         token: str,
         address_filter: List[str] | None,
         start: int | datetime,
@@ -61,10 +69,13 @@ class ERC20Data:
         self._erc20_metas_service = erc20_metas_service
         self._events_service = events_service
         self._blocks_service = blocks_service
+        self._calls_service = calls_service
         self._grid_step = grid_step
 
         self._meta = None
         self._transfers = None
+        self._token_contract = None
+        self._chain_id = None
 
     @staticmethod
     def create(
@@ -94,6 +105,7 @@ class ERC20Data:
             w3 = Web3(Web3.HTTPProvider(rpc))
         events_service = EventsService.create(cache_path)
         blocks_service = BlocksService.create(cache_path, rpc)
+        calls_service = CallsService.create(cache_path)
         erc20_metas_service = ERC20MetasService.create(cache_path, rpc)
 
         return ERC20Data(
@@ -101,6 +113,7 @@ class ERC20Data:
             erc20_metas_service=erc20_metas_service,
             events_service=events_service,
             blocks_service=blocks_service,
+            calls_service=calls_service,
             token=token,
             address_filter=address_filter,
             start=start,
@@ -138,20 +151,136 @@ class ERC20Data:
             ).number
         return self._to_block
 
-    def _build_transfers(self):
-        current_folder = os.path.realpath(os.path.dirname(__file__))
-        erc20_abi = None
-        with open(f"{current_folder}/erc20_abi.json", "r") as f:
-            erc20_abi = json.load(f)
-        chain_id = self._w3.eth.chain_id
-        token: Contract = self._w3.eth.contract(
-            address=self._w3.toChecksumAddress(self.meta.address), abi=erc20_abi
+    @property
+    def volumes(self) -> pl.DataFrame:
+        df1 = (
+            self.transfers[["from", "value"]]
+            .groupby("from")
+            .agg(pl.col("value").sum())
+            .rename({"from": "address"})
         )
+        df2 = (
+            self.transfers[["to", "value"]]
+            .groupby("to")
+            .agg(pl.col("value").sum())
+            .rename({"to": "address"})
+        )
+        return (
+            df1.extend(df2)
+            .groupby("address")
+            .agg(pl.col("value").sum())
+            .sort(pl.col("value"), reverse=True)
+        )
+
+    @property
+    def net_volumes(self) -> pl.DataFrame:
+        df1 = (
+            self.transfers[["from", "value"]]
+            .groupby("from")
+            .agg(pl.col("value").sum())
+            .rename({"from": "address"})
+        )
+        df2 = (
+            self.transfers[["to", "value"]]
+            .groupby("to")
+            .agg(pl.col("value").sum())
+            .rename({"to": "address"})
+        ).with_column((pl.col("value") * (-1)).alias("value"))
+        return (
+            df1.extend(df2)
+            .groupby("address")
+            .agg(pl.col("value").sum())
+            .sort(pl.col("value"), reverse=True)
+        )
+
+    @property
+    def chain_id(self) -> int:
+        if self._chain_id is None:
+            self._chain_id = self._w3.eth.chain_id
+        return self._chain_id
+
+    @property
+    def token_contract(self) -> Contract:
+        current_folder = os.path.realpath(os.path.dirname(__file__))
+        if self._token_contract is None:
+            with open(f"{current_folder}/erc20_abi.json", "r") as f:
+                erc20_abi = json.load(f)
+            self._token_contract = self._w3.eth.contract(
+                address=self._w3.toChecksumAddress(self.meta.address), abi=erc20_abi
+            )
+        return self._token_contract
+
+    def balances(
+        self,
+        address: str,
+        timestamps: List[int | datetime],
+        initial_balance: int | None = None,
+    ) -> pl.DataFrame:
+        if initial_balance is None:
+            first_block = self.transfers["block_number"][0]
+            initial_balance_wei = self._calls_service.get_call(
+                self.chain_id,
+                self.token_contract.functions.balanceOf(
+                    Web3.toChecksumAddress(address)
+                ),
+                first_block - 1,
+            ).response
+            initial_balance = initial_balance_wei / 10**self.meta.decimals
+
+        for i in range(len(timestamps)):
+            ts = timestamps[i]
+            # resolve datetimes to timestamps
+            if isinstance(ts, datetime):
+                timestamps[i] = int(time.mktime(ts.timetuple()))
+        timestamps = sorted(timestamps)
+
+        bs = self._zero_balances(address, timestamps)
+        res = [
+            {
+                "timestamp": ts,
+                "date": datetime.fromtimestamp(ts),
+                "balance": balance + initial_balance,
+            }
+            for ts, balance in zip(timestamps, bs)
+        ]
+        return pl.DataFrame(res)
+
+    def _zero_balances(
+        self, address: str, timestamps: List[int | datetime]
+    ) -> List[np.float64]:
+        if len(timestamps) == 0:
+            return []
+
+        address = address.lower()
+        transfers = (
+            self.transfers.filter(
+                (pl.col("from") == address) | (pl.col("to") == address)
+            )
+            .filter((pl.col("from") != address) | (pl.col("to") != address))
+            .with_column(
+                pl.when(pl.col("from") == address)
+                .then(pl.col("value") * (-1))
+                .otherwise(pl.col("value"))
+                .alias("cash_flow")
+            )[["timestamp", "cash_flow"]]
+            .to_dicts()
+        )
+        j = 0
+        balance = 0
+        res = []
+        for ts in timestamps:
+            while j < len(transfers) and transfers[j]["timestamp"] <= ts:
+                balance += transfers[j]["cash_flow"]
+                j += 1
+            res.append(balance)
+        return res
+
+    def _build_transfers(self):
         events = []
         for filters in self._build_argument_filters():
             fetched_events = self._events_service.get_events(
-                chain_id,
-                token.events.Transfer,
+                self.chain_id,
+                self.token_contract.events.Transfer,
                 self.from_block,
                 self.to_block,
                 argument_filters=filters,
@@ -169,7 +298,7 @@ class ERC20Data:
         factor = 10**self.meta.decimals
         self._transfers = pl.from_dicts(
             [self._event_to_row(e, ts_index[e.block_number], factor) for e in events]
-        )
+        ).sort(pl.col("timestamp"))
 
     def _build_argument_filters(
         self,
