56e10f2af0e2bec6097e6d29ea2f4fcf7e33730e
mono6 <monowhile6@gmail.com>
2022-11-08 12:34:49 +0400

    fix

--- data/__init__.py
+++ data/__init__.py
@@ -4,6 +4,7 @@ from bokeh.io import curdoc
 pl.Config.set_fmt_str_lengths(44)
 curdoc().theme = "dark_minimal"
 
-from data.views import DataView
 from data.erc20s import ERC20Data
 from data.chainlink.chainlink_data import ChainlinkUSDData, ChainlinkData
+from data.ethers import EtherData
+from data.portfolios import PortfolioData
